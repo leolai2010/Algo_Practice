decc9ecd70f1603769b99694234519cd339ad4b8
Leo Lai <38085012+leolai2010@users.noreply.github.com>
2018-10-24 05:32:53 -0700

    taking small break

--- UdemyAlgo/exercises/anagrams/index.js
+++ UdemyAlgo/exercises/anagrams/index.js
@@ -8,6 +8,100 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB) {
+    stringA = stringA.replace(/[^\w]/g, '').toLowerCase();
+    stringB = stringB.replace(/[^\w]/g, '').toLowerCase();
+    const Adic={};
+    const Bdic={};
+    for(let i of stringA){
+      if(stringA[i]){
+        Adic[i]++;
+      } else {
+        Adic[i] = 1;
+      }
+    }
+    for(let j of stringB){
+      if(stringB[j]){
+        Bdic[j]++;
+      } else {
+        Bdic[j] = 1;
+      }
+    }
+    function isEquivalent(a, b) {
+        // Create arrays of property names
+        var aProps = Object.getOwnPropertyNames(a);
+        var bProps = Object.getOwnPropertyNames(b);
 
+        // If number of properties is different,
+        // objects are not equivalent
+        if (aProps.length != bProps.length) {
+            return false;
+        }
+
+        for (var i = 0; i < aProps.length; i++) {
+            var propName = aProps[i];
+
+            // If values of same property are not equal,
+            // objects are not equivalent
+            if (a[propName] !== b[propName]) {
+                return false;
+            }
+        }
+
+        // If we made it this far, objects
+        // are considered equivalent
+        return true;
+    }
+ return isEquivalent(Adic,Bdic)
+}
+//steven's solution
+function anagrams(stringA, stringB){
+    const Amap = buildCharMap(stringA)
+    const Bmap = buildCharMap(stringB)
+    if(Object.keys(Amap).length != Object.keys(Bmap).length){
+        return false;
+    }
+    for(let j in Amap){
+        if(Amap[j] !== Bmap[j]){
+            return false;
+        }
+    }
+    return true;
+    
+}
+function buildCharMap(str){
+    const charMap = {};
+    for(let i of str.replace(/[^\w]/g, '').toLowerCase()){
+        if(str[i]){
+            charMap[i]++;
+        } else {
+            charMap[i] = 1;
+        }
+    }
+    return charMap
+}
+//my way using built-in functions
+function anagrams(stringA, stringB){
+    stringA = stringA.replace(/[^\w]/g, '').toLowerCase().split("").sort()
+    stringB = stringB.replace(/[^\w]/g, '').toLowerCase().split("").sort()
+    if (stringA.length !== stringB.length){
+      return false;
+    }
+    for(let i of stringA){
+      if(stringA[i] != stringB[i]){
+        return false;
+      }
+    }
+    return true;
+}
+//steven's solution
+function anagrams(stringA, stringB){
+    cleanString(stringA) === cleanString(stringB);
+}
+function cleanString(str){
+    return str.replace(/[^\w]/g, '').toLowerCase().split('').sort().join('')
+}
+
+
+          
 module.exports = anagrams;
--- UdemyAlgo/exercises/capitalize/index.js
+++ UdemyAlgo/exercises/capitalize/index.js
@@ -7,6 +7,32 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
-
+//my solution
+function capitalize(str) {
+    const newarr = []
+    str.split(" ").forEach((element)=>{
+       newarr.push(element.charAt(0).toUpperCase() + element.slice(1))
+    })
+    return newarr.join(' ')
+  }
+//steven's solution 
+function capitalize(str){
+    const words = [];
+    for(let word of str.split(' ')){
+        words.push(word[0].toUpperCase() + word.slice(1))
+    }
+    return words.join(' ')
+}
+//steven's alternative solution
+function capitalize(str){
+    let result = str[0].toUpperCase();
+    for(let i = 1; i<str.length; i++){
+        if(str[i - 1] === ' '){
+            result += str[i].toUpperCase()
+        } else {
+            result += str[i]
+        }
+    }
+    return result
+}
 module.exports = capitalize;
